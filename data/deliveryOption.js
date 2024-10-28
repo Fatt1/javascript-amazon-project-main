57543639fea2e85c6b51c4aedb30f8074650ec80
Fatt1 <phat3022005@gmail.com>
2024-10-28 22:47:56 +0700

    Save data and generate HML for delivery options

---
+++ data/deliveryOption.js
@@ -0,0 +1,19 @@
+export const deliveryOptions = [
+  {
+    id: '1',
+    deliveryDate: 7,
+    priceCents: 0
+  },
+
+  {
+    id: '2',
+    deliveryDate: 3,
+    priceCents: 399
+  },
+
+  {
+    id: '3',
+    deliveryDate: 1,
+    priceCents: 799
+  }
+]
